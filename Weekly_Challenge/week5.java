1dfee2a18a4d8b4a5f6872b5411ebfd6ef65d399
jihyunhillpark <girl9505@naver.com>
2021-09-03 23:00:36 +0900

    code : week5 모음 사전 - DFS사용

---
+++ Weekly_Challenge/week5.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    static char vowels[] = {'A','E','I','O','U'};
+    static int ret;
+    static boolean flag = true;
+    public int solution(String word) {
+        int answer = 0;
+        
+        answer = dfs("",word,0);
+    
+        return answer;
+    }
+    static int dfs(String now, String word, int order){
+        if(now.equals(word)) {
+            flag = false;
+            return order;
+        }
+        if(5 <= now.length()) return order;
+        for(int i = 0; i < 5 && flag ; i++){
+            // System.out.println(now+vowels[i] + " " + order + flag);
+            order = dfs(now + vowels[i], word, order+1);
+        }
+        return order;
+    }
+}
